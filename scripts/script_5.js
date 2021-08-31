51078b06903b0ce5c641e82eb574f7daa8896c42
Patrick Genier <patrick.j.genier@gmail.com>
2021-08-30 21:36:44 -0400

    8-30-21-1stCom

--- scripts/script_5.js
+++ scripts/script_5.js
@@ -17,3 +17,13 @@ function myHamburg() {
   function closeNav() {
     document.getElementById("myNav5").style.width = "0%";
   }
+
+
+  //-------------------- Shareto Facebook --------------------
+  (function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = "https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v3.0";
+    fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
