aef176802e533d37fe47dad67b2c471973841455
Patrick Genier <patrick.j.genier@gmail.com>
2021-09-15 18:44:32 -0400

    9-15-21 searchBarAdded

--- scripts/script_5.js
+++ scripts/script_5.js
@@ -16,12 +16,23 @@ function closeNav() {
   document.getElementById("myNav5").style.width = "0%";
 }
 
-/*-----------------------------------------------------
-(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = "https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v3.0";
-  fjs.parentNode.insertBefore(js, fjs);
-  }(document, 'script', 'facebook-jssdk'));
------------------------------------------------------*/
+//-------------------- Filter Articles --------------------
+function myFilterFunction() {
+  // Declare variables
+  var input, filter, ul, li, a, i, txtValue;
+  input = document.getElementById('searchInput1');
+  filter = input.value.toUpperCase();
+  ul = document.getElementById("myUL");
+  li = ul.getElementsByTagName('li');
+
+  // Loop through all list items, and hide those who don't match the search query
+  for (i = 0; i < li.length; i++) {
+    a = li[i].getElementsByTagName("a")[1];
+    txtValue = a.textContent || a.innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      li[i].style.display = "";
+    } else {
+      li[i].style.display = "none";
+    }
+  }
+}
