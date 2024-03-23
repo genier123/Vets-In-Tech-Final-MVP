3c3d0c2dafdafb1c40317c116bd912ce08b0de62
Patrick Genier <patrick.j.genier@gmail.com>
2024-03-23 17:08:19 -0400

    namefix_1

--- scripts/script_4.js
+++
@@ -1,9 +0,0 @@
-  //-------------------- NewMenuButton --------------------
-
-  function openNav() {
-    document.getElementById("myNav5").style.width = "100%";
-  }
-  
-  function closeNav() {
-    document.getElementById("myNav5").style.width = "0%";
-  }
