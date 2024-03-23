43ae6dbc3efd71c09d389b1a795ba0a3c3bb95b7
Patrick Genier <patrick.j.genier@gmail.com>
2024-03-23 18:45:01 -0400

    namechanges2

--- scripts/100-Wedding-Songs-for-Every-Wedding.js
+++ scripts/100-Wedding-Songs-for-Every-Wedding.js
@@ -82,168 +82,192 @@ let songData = [
     { Artist: "Dan + Shay", Song: "From the Ground Up" },
     { Artist: "Colbie Caillat", Song: "I Do" },
     { Artist: "Tom Walker", Song: "Just You and I" },
+    { Artist: "The Wannadies", Song: "You and Me Song" },
+    { Artist: "James Bay", Song: "Us" },
+    { Artist: "Tom Odell", Song: "Grow Old with Me" },
+    { Artist: "Daniel Caesar ft. H.E.R.", Song: "Best Part" },
+    { Artist: "The Beatles", Song: "Something" },
+    { Artist: "Bette Midler", Song: "The Rose" },
+    { Artist: "Jackie DeShannon", Song: "What the World Needs Now Is Love" },
+    { Artist: "Al Green", Song: "Let's Stay Together" },
+    { Artist: "Sixpence None the Richer", Song: "Kiss Me" },
+    { Artist: "Jackson 5", Song: "I Want You Back" },
+    { Artist: "Lady Antebellum", Song: "Can't Take My Eyes Off You" },
+    { Artist: "Queen", Song: "You're My Best Friend" },
+    { Artist: "Elton John", Song: "Something About the Way You Look Tonight" },
+    { Artist: "The Beatles", Song: "In My Life" },
+    { Artist: "Lady Gaga", Song: "Always Remember Us This Way" },
+    { Artist: "Ed Sheeran & Andrea Bocelli", Song: "Perfect Symphony" },
+    { Artist: "Calum Scott", Song: "You Are the Reason" },
+    { Artist: "Ben Folds", Song: "The Luckiest" },
+    { Artist: "Taylor Swift", Song: "Lover" },
+    { Artist: "Edward Sharpe & The Magnetic Zeros", Song: "Home" },
+    { Artist: "Kina Grannis", Song: "Can't Help Falling in Love" },
+    { Artist: "Sleeping At Last", Song: "Make You Feel My Love" },
+    { Artist: "Ruelle", Song: "I Get to Love You" },
+    { Artist: "Vance Joy", Song: "Just You and I" },
 ];
 
-  function generateTableHead(table, data) {
-    let thead = table.createTHead();
-    let row = thead.insertRow();
-    for (let key of data) {
-      let th = document.createElement("th");
-      let text = document.createTextNode(key);
-      th.appendChild(text);
-      row.appendChild(th);
+function generateTableHead(table, data) {
+  let thead = table.createTHead();
+  let row = thead.insertRow();
+  for (let key of data) {
+    let th = document.createElement("th");
+    let text = document.createTextNode(key);
+    th.appendChild(text);
+    row.appendChild(th);
+  }
+}
+
+function generateTable(table, data) {
+  for (let element of data) {
+    let row = table.insertRow();
+    for (key in element) {
+      let cell = row.insertCell();
+      let text = document.createTextNode(element[key]);
+      cell.appendChild(text);
     }
   }
-  
-  function generateTable(table, data) {
-    for (let element of data) {
-      let row = table.insertRow();
-      for (key in element) {
-        let cell = row.insertCell();
-        let text = document.createTextNode(element[key]);
-        cell.appendChild(text);
+}
+
+let table = document.querySelector("table");
+let data = Object.keys(songData[0]);
+generateTableHead(table, data);
+generateTable(table, songData);
+
+//-------------------- Filter Artist --------------------
+  function myFunctionA() {
+    var filter = event.target.value.toUpperCase();
+    var rows = document.querySelector("#myTable").rows;
+    
+    for (var i = 0; i < rows.length; i++) {
+        var firstCol = rows[i].cells[0].textContent.toUpperCase();
+        var secondCol = rows[i].cells[1].textContent.toUpperCase();
+        if (firstCol.indexOf(filter) > -1 || secondCol.indexOf(filter) > -1) {
+            rows[i].style.display = "";
+        } else {
+            rows[i].style.display = "none";
+        }      
+    }
+}
+
+//--------------------- Sort Artist ---------------------
+function sortTableA() {
+  var table, rows, switching, i, x, y, shouldSwitch;
+  table = document.getElementById("myTable");
+  switching = true;
+  /*Make a loop that will continue until
+  no switching has been done:*/
+  while (switching) {
+    //start by saying: no switching is done:
+    switching = false;
+    rows = table.rows;
+    /*Loop through all table rows (except the
+    first, which contains table headers):*/
+    for (i = 1; i < (rows.length - 1); i++) {
+      //start by saying there should be no switching:
+      shouldSwitch = false;
+      /*Get the two elements you want to compare,
+      one from current row and one from the next:*/
+      x = rows[i].getElementsByTagName("TD")[0];
+      y = rows[i + 1].getElementsByTagName("TD")[0];
+      //check if the two rows should switch place:
+      if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+        //if so, mark as a switch and break the loop:
+        shouldSwitch = true;
+        break;
       }
     }
-  }
-  
-  let table = document.querySelector("table");
-  let data = Object.keys(songData[0]);
-  generateTableHead(table, data);
-  generateTable(table, songData);
-  
-  //-------------------- Filter Artist --------------------
-    function myFunctionA() {
-      var filter = event.target.value.toUpperCase();
-      var rows = document.querySelector("#myTable").rows;
-      
-      for (var i = 0; i < rows.length; i++) {
-          var firstCol = rows[i].cells[0].textContent.toUpperCase();
-          var secondCol = rows[i].cells[1].textContent.toUpperCase();
-          if (firstCol.indexOf(filter) > -1 || secondCol.indexOf(filter) > -1) {
-              rows[i].style.display = "";
-          } else {
-              rows[i].style.display = "none";
-          }      
-      }
-  }
-  
-  //--------------------- Sort Artist ---------------------
-  function sortTableA() {
-    var table, rows, switching, i, x, y, shouldSwitch;
-    table = document.getElementById("myTable");
-    switching = true;
-    /*Make a loop that will continue until
-    no switching has been done:*/
-    while (switching) {
-      //start by saying: no switching is done:
-      switching = false;
-      rows = table.rows;
-      /*Loop through all table rows (except the
-      first, which contains table headers):*/
-      for (i = 1; i < (rows.length - 1); i++) {
-        //start by saying there should be no switching:
-        shouldSwitch = false;
-        /*Get the two elements you want to compare,
-        one from current row and one from the next:*/
-        x = rows[i].getElementsByTagName("TD")[0];
-        y = rows[i + 1].getElementsByTagName("TD")[0];
-        //check if the two rows should switch place:
-        if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
-          //if so, mark as a switch and break the loop:
-          shouldSwitch = true;
-          break;
-        }
-      }
-      if (shouldSwitch) {
-        /*If a switch has been marked, make the switch
-        and mark that a switch has been done:*/
-        rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
-        switching = true;
-      }
+    if (shouldSwitch) {
+      /*If a switch has been marked, make the switch
+      and mark that a switch has been done:*/
+      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+      switching = true;
     }
   }
-  
-  //------------------- Sort Song Name ------------------
-  function sortTableS() {
-    var table, rows, switching, i, x, y, shouldSwitch;
-    table = document.getElementById("myTable");
-    switching = true;
-    /*Make a loop that will continue until
-    no switching has been done:*/
-    while (switching) {
-      //start by saying: no switching is done:
-      switching = false;
-      rows = table.rows;
-      /*Loop through all table rows (except the
-      first, which contains table headers):*/
-      for (i = 1; i < (rows.length - 1); i++) {
-        //start by saying there should be no switching:
-        shouldSwitch = false;
-        /*Get the two elements you want to compare,
-        one from current row and one from the next:*/
-        x = rows[i].getElementsByTagName("TD")[1];
-        y = rows[i + 1].getElementsByTagName("TD")[1];
-        //check if the two rows should switch place:
-        if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
-          //if so, mark as a switch and break the loop:
-          shouldSwitch = true;
-          break;
-        }
-      }
-      if (shouldSwitch) {
-        /*If a switch has been marked, make the switch
-        and mark that a switch has been done:*/
-        rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
-        switching = true;
+}
+
+//------------------- Sort Song Name ------------------
+function sortTableS() {
+  var table, rows, switching, i, x, y, shouldSwitch;
+  table = document.getElementById("myTable");
+  switching = true;
+  /*Make a loop that will continue until
+  no switching has been done:*/
+  while (switching) {
+    //start by saying: no switching is done:
+    switching = false;
+    rows = table.rows;
+    /*Loop through all table rows (except the
+    first, which contains table headers):*/
+    for (i = 1; i < (rows.length - 1); i++) {
+      //start by saying there should be no switching:
+      shouldSwitch = false;
+      /*Get the two elements you want to compare,
+      one from current row and one from the next:*/
+      x = rows[i].getElementsByTagName("TD")[1];
+      y = rows[i + 1].getElementsByTagName("TD")[1];
+      //check if the two rows should switch place:
+      if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+        //if so, mark as a switch and break the loop:
+        shouldSwitch = true;
+        break;
       }
     }
+    if (shouldSwitch) {
+      /*If a switch has been marked, make the switch
+      and mark that a switch has been done:*/
+      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+      switching = true;
+    }
   }
-  
-  //---------------- Random Add Space --------------------------
-  /*
-  window.onload = choosePic;
-  
-  var myPix = new Array("./addimages/BlankAdd1.png","./addimages/BlankAdd2.png","./addimages/BlankAdd3.png","./addimages/BlankAdd4.png");
-  
-  function choosePic() {
-  var randomNum = Math.floor(Math.random() * myPix.length);
-  document.getElementById("myPicture1").src = myPix[randomNum];
-  }
-  */
-  //---------------- Hamburg Menu --------------------------
-  function myHamburg() {
-  var x = document.getElementById("myLinks");
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "block";
-  }
-  }
-  
-  //---------------- Scroll Down --------------------------
-  // When the user scrolls the page, execute myFunction
-  window.onscroll = function() {myScrolldown()};
-  
-  function myScrolldown() {
-  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-  var scrolled = (winScroll / height) * 100;
-  document.getElementById("myBar").style.width = scrolled + "%";
-  }
-  
-  //---------------- Pop Up Model --------------------------
-  function myPopup() {
-  var popup = document.getElementById("myPopup");
-  popup.classList.toggle("show");
-  }
-  
-  
-  //-------------------- NewMenuButton --------------------
-  
-  function openNav() {
-    document.getElementById("myNav5").style.width = "100%";
-  }
-  
-  function closeNav() {
-    document.getElementById("myNav5").style.width = "0%";
-  }
+}
+
+//---------------- Random Add Space --------------------------
+/*
+window.onload = choosePic;
+
+var myPix = new Array("./addimages/BlankAdd1.png","./addimages/BlankAdd2.png","./addimages/BlankAdd3.png","./addimages/BlankAdd4.png");
+
+function choosePic() {
+var randomNum = Math.floor(Math.random() * myPix.length);
+document.getElementById("myPicture1").src = myPix[randomNum];
+}
+*/
+//---------------- Hamburg Menu --------------------------
+function myHamburg() {
+var x = document.getElementById("myLinks");
+if (x.style.display === "block") {
+  x.style.display = "none";
+} else {
+  x.style.display = "block";
+}
+}
+
+//---------------- Scroll Down --------------------------
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myScrolldown()};
+
+function myScrolldown() {
+var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+var scrolled = (winScroll / height) * 100;
+document.getElementById("myBar").style.width = scrolled + "%";
+}
+
+//---------------- Pop Up Model --------------------------
+function myPopup() {
+var popup = document.getElementById("myPopup");
+popup.classList.toggle("show");
+}
+
+
+//-------------------- NewMenuButton --------------------
+
+function openNav() {
+  document.getElementById("myNav5").style.width = "100%";
+}
+
+function closeNav() {
+  document.getElementById("myNav5").style.width = "0%";
+}
